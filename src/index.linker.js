65cae0916b57ba14178a6b6bc997315bd81343d3
homemovie <153278990+omidomidvari@users.noreply.github.com>
2026-03-01 12:24:40 -0800

    Update index.linker.js

--- src/index.linker.js
+++ src/index.linker.js
@@ -1,1 +1,7 @@
-console.something
+import flowjs from lib/flowjs/flow.js
+function onstart {
+console.log(" flow has been imported");
+flowjs.bulklinker(65, 78, 89, bulk);
+flowjs.trans(0f, 0b, 6v);
+}
+onstart()
